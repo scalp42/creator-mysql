cd51e85004d2be947d501d79e9882b1f670f20fc
Anthony Scalisi <scalisi.a@gmail.com>
2016-01-05 18:11:47 -0800

    format logger

--- creator.rb
+++ creator.rb
@@ -9,12 +9,12 @@ logger.level = Logger::INFO
 if ENV['CREATOR_MYSQL_DATABASES']
   dbs = ENV.fetch('CREATOR_MYSQL_DATABASES').split('|')
 else
-  logger.info %|creator-mysql => CREATOR_MYSQL_DATABASES variable does not exist! Bailing.|
+  logger.info('creator-mysql') { %| => CREATOR_MYSQL_DATABASES variable does not exist! Bailing.| }
   exit 1
 end
 
 begin
-  logger.info %|creator-mysql => Connecting to #{ENV.fetch('CREATOR_MYSQL_HOST', 'localhost')}...|
+  logger.info('creator-mysql') { %| => Connecting to #{ENV.fetch('CREATOR_MYSQL_HOST', 'localhost')}...| }
   c = Mysql.connect(
     ENV.fetch('CREATOR_MYSQL_HOST', 'localhost'),
         ENV.fetch('CREATOR_MYSQL_USER', 'root'),
@@ -33,17 +33,22 @@ begin
     mysql_flush = ['FLUSH PRIVILEGES;']
 
     %W|#{mysql_create.join("\n")} #{mysql_grant.join("\n")} #{mysql_flush.join("\n")}|.each do |query|
-      logger.info %|creator-mysql => Setting up #{db}...|
+      logger.info('creator-mysql') { %| => Setting up #{db}...| }
       c.query(query)
-      logger.info %|creator-mysql => ...done.|
+      logger.info('creator-mysql') { %| => ...done.| }
     end
   end
 
+  logger.close
+
   if ENV['CREATOR_MARATHON']
     # Hack not have the app exit under Marathon
-    logger.info %|creator-mysql => CREATOR_MARATHON specified, sleeping forever.|
+    logger.info('creator-mysql') { %| => CREATOR_MARATHON specified, sleeping forever.| }
+    logger.close
     sleep
   end
 rescue Mysql::ServerError::AccessDeniedError, Errno::ETIMEDOUT => ex
-  logger.info %|creator-mysql => #{ex.message}|
+  logger.info('creator-mysql') { %| => #{ex.message}| }
+ensure
+  logger.close
 end
