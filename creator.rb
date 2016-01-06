57d69da024aa199302f501de504d017cbb435353
Anthony Scalisi <scalisi.a@gmail.com>
2016-01-05 17:33:24 -0800

    add some rescue

--- creator.rb
+++ creator.rb
@@ -9,30 +9,37 @@ else
   exit 1
 end
 
-c = Mysql.connect(
-  ENV.fetch('CREATOR_MYSQL_HOST', 'localhost'),
-      ENV.fetch('CREATOR_MYSQL_USER', 'root'),
-      ENV.fetch('CREATOR_MYSQL_PASS', nil)
-)
+begin
+  puts %|#{__method__.to_s} => Connecting to #{ENV.fetch('CREATOR_MYSQL_HOST', 'localhost')}...|
+  c = Mysql.connect(
+    ENV.fetch('CREATOR_MYSQL_HOST', 'localhost'),
+        ENV.fetch('CREATOR_MYSQL_USER', 'root'),
+        ENV.fetch('CREATOR_MYSQL_PASS', nil)
+  )
 
-dbs.each do |db|
-  mysql_create = [%|CREATE DATABASE IF NOT EXISTS #{db}|]
-  mysql_create << '  CHARACTER SET utf8'
-  mysql_create << '  DEFAULT CHARACTER SET utf8'
-  mysql_create << '  COLLATE utf8_general_ci'
-  mysql_create << '  DEFAULT COLLATE utf8_general_ci;'
+  dbs.each do |db|
+    mysql_create = [%|CREATE DATABASE IF NOT EXISTS #{db}|]
+    mysql_create << '  CHARACTER SET utf8'
+    mysql_create << '  DEFAULT CHARACTER SET utf8'
+    mysql_create << '  COLLATE utf8_general_ci'
+    mysql_create << '  DEFAULT COLLATE utf8_general_ci;'
 
-  mysql_grant = [%|GRANT ALL PRIVILEGES ON #{db}.* TO '#{db.gsub(/\d\s?/, '')}user'@'%' IDENTIFIED BY 'dev';|]
+    mysql_grant = [%|GRANT ALL PRIVILEGES ON #{db}.* TO '#{db.gsub(/\d\s?/, '')}user'@'%' IDENTIFIED BY 'dev';|]
 
-  mysql_flush = ['FLUSH PRIVILEGES;']
+    mysql_flush = ['FLUSH PRIVILEGES;']
 
-  %W|#{mysql_create.join("\n")} #{mysql_grant.join("\n")} #{mysql_flush.join("\n")}|.each do |query|
-    c.query(query)
+    %W|#{mysql_create.join("\n")} #{mysql_grant.join("\n")} #{mysql_flush.join("\n")}|.each do |query|
+      puts %|#{__method__.to_s} => Setting up #{db}...|
+      c.query(query)
+      puts %|#{__method__.to_s} => ...done.|
+    end
   end
-end
 
-if ENV['CREATOR_MARATHON']
-  # Hack not have the app exit under Marathon
-  puts %|#{__method__.to_s} => CREATOR_MARATHON specified, sleeping forever.|
-  sleep
+  if ENV['CREATOR_MARATHON']
+    # Hack not have the app exit under Marathon
+    puts %|#{__method__.to_s} => CREATOR_MARATHON specified, sleeping forever.|
+    sleep
+  end
+rescue Mysql::ServerError::AccessDeniedError, Errno::ETIMEDOUT => ex
+  puts %|#{__method__.to_s} => #{ex.message}|
 end
