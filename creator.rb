cfd0c05db699aa0a115d7ba509335b2b4b169939
Anthony Scalisi <scalisi.a@gmail.com>
2016-01-06 17:26:41 -0800

    tweak logger, close mysql connection

--- creator.rb
+++ creator.rb
@@ -7,17 +7,20 @@ $stdout.sync = true
 
 logger = Logger.new(STDOUT)
 logger.level = Logger::INFO
+logger.formatter = proc do |severity, datetime, progname, msg|
+  "[#{datetime}] #{severity} creator-mysql: #{msg}\n"
+end
 
 if ENV['CREATOR_MYSQL_DATABASES']
   dbs = ENV.fetch('CREATOR_MYSQL_DATABASES').split('|')
 else
-  logger.info('creator-mysql') { %| => CREATOR_MYSQL_DATABASES variable does not exist! Bailing.| }
+  logger.info { %|=> CREATOR_MYSQL_DATABASES variable does not exist! Bailing.| }
   exit 1
 end
 
 tries ||= ENV.fetch('CREATOR_MYSQL_TRIES', 1).to_i
 begin
-  logger.info('creator-mysql') { %| => Connecting to #{ENV.fetch('CREATOR_MYSQL_HOST', 'localhost')}...| }
+  logger.info { %|=> Connecting to #{ENV.fetch('CREATOR_MYSQL_HOST', 'localhost')}...| }
   c = Mysql.connect(
     ENV.fetch('CREATOR_MYSQL_HOST', 'localhost'),
         ENV.fetch('CREATOR_MYSQL_USER', 'root'),
@@ -36,25 +39,26 @@ begin
     mysql_flush = ['FLUSH PRIVILEGES;']
 
     %W|#{mysql_create.join("\n")} #{mysql_grant.join("\n")} #{mysql_flush.join("\n")}|.each do |query|
-      logger.info('creator-mysql') { %| => Setting up #{db}...| }
+      logger.info { %|=> Setting up #{db}...| }
       c.query(query)
-      logger.info('creator-mysql') { %| => ...done.| }
+      logger.info { %|=> ...done.| }
+      c.close
     end
   end
 
-  logger.close
+  logger.close unless ENV['CREATOR_MARATHON']
 
   if ENV['CREATOR_MARATHON']
     # Hack not have the app exit under Marathon
-    logger.info('creator-mysql') { %| => CREATOR_MARATHON specified, sleeping forever.| }
+    logger.info { %|=> CREATOR_MARATHON specified, sleeping forever.| }
     logger.close
     sleep
   end
 rescue Mysql::ServerError::AccessDeniedError, Errno::ETIMEDOUT => ex
-  logger.info('creator-mysql') { %| => #{ex.message}| }
+  logger.info { %|=> #{ex.message}| }
 rescue SocketError => ex
-  logger.warn('creator-mysql') { %| => #{ex.message}| }
-  logger.warn('creator-mysql') { %| => Sleeping for #{ENV.fetch('CREATOR_MYSQL_SLEEP', 10)} seconds, attempts left #{tries}/#{ENV.fetch('CREATOR_MYSQL_TRIES', 1)}| }
+  logger.warn { %|=> #{ex.message}| }
+  logger.warn { %|=> Sleeping for #{ENV.fetch('CREATOR_MYSQL_SLEEP', 10)} seconds, attempts left #{tries}/#{ENV.fetch('CREATOR_MYSQL_TRIES', 1)}| }
   sleep ENV.fetch('CREATOR_MYSQL_SLEEP', 10).to_i
   retry unless (tries -= 1).zero?
 ensure
